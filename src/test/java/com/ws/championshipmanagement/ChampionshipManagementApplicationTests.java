16c34186bed5bf2d45bd4a7467e11b3838fb93e3
florian kassi <kassi.florian@gmail.com>
2025-03-20 16:08:45 +0100

    Clean architecture and project initialization

---
+++ src/test/java/com/ws/championshipmanagement/ChampionshipManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.championshipmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionshipManagementApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
